f3d4a199fef6f17e9265508c5c6655ce7ed1d0b9
jennifer cuevas <jenni_fca24@hotmail.com>
2018-10-04 15:18:43 -0500

    Modificacion del jTable

--- MVCTutorial/MVCTutorial/Controllers/HomeController.cs
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCTutorial.Models;
 
 namespace MVCTutorial.Controllers
 {
@@ -30,6 +31,18 @@ namespace MVCTutorial.Controllers
         public ActionResult SideMenu()
         {
             return PartialView("SideMenu");
+        }
+
+        public JsonResult GetEmployeeRecord()
+        {
+            MVCTutorialEntities1 db = new MVCTutorialEntities1();
+            List<EmployeeViewModel> List = db.Employees.Select(x=>new EmployeeViewModel { Name = x.Name,
+                EmployeeId = x.EmployeeId,
+                DepartmentId = x.DepartmentId,
+                DepartmentName = x.Department.DepartmentName,
+                Address = x.Addres,
+                IsDeleted = x.IsDeleted }).ToList();
+            return Json("", JsonRequestBehavior.AllowGet);
         }
     }
 }
