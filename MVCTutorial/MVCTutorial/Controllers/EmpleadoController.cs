99ffbefc5428466d99295247eaaa8e84dbc90ed3
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2018-10-04 22:13:27 -0500

    primer cambio

---
+++ MVCTutorial/MVCTutorial/Controllers/EmpleadoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            //esta modificacion la estoy haciendo desde 
+            return View();
+        }
+    }
+}
